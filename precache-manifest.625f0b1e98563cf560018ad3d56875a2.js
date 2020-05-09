ab57d38729cd2c6aea1d7e5d716849b210205ad6
Harshil Goel <HarshilGoel@example.com>
2020-05-09 22:37:08 +0530

    Updates

---
+++ precache-manifest.625f0b1e98563cf560018ad3d56875a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5851d78e109df0716add56fc6e1b42fe",
+    "url": "/covid19World/index.html"
+  },
+  {
+    "revision": "20e816e33d19edb7d68f",
+    "url": "/covid19World/static/css/main.bf4864d5.chunk.css"
+  },
+  {
+    "revision": "b1eb07412aa1475caf8a",
+    "url": "/covid19World/static/js/2.c196429a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19World/static/js/2.c196429a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e816e33d19edb7d68f",
+    "url": "/covid19World/static/js/main.dba29bcf.chunk.js"
+  },
+  {
+    "revision": "5b345be13a6d08cfd0e7",
+    "url": "/covid19World/static/js/runtime-main.82ba2f52.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19World/static/media/image.d7265326.png"
+  }
+]);
