753a3647d746dd997991a244506a3f0e41642e26
Harshil Goel <harshilgoel96@gmail.com>
2020-09-12 14:37:12 +0530

    Changes to the layout

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import styles from './Cards.module.css';
-import CountUp from 'react-countup';
-import cx from 'classnames';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
+import CountUp from "react-countup";
+import cx from "classnames";
 
 class Cards extends React.Component {
-
-    render() {
-        const { data } = this.props;
-        //Necessary to handle this using if else otherwise will get undefind error
-        if (!data.confirmed) {
-            return <h1>Loading Data, Please Wait...........</h1>;
-        }
-        else {
-            return (
-                <div className={styles.container}>
-                    <Grid container spacing={2} justify="center" >
-                        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                            <CardContent>
-                                <Typography color="textPrimary" gutterBottom>Confirmed</Typography>
-                                <Typography variant="h5">
-                                    <CountUp start={0} end={data.confirmed.value} duration={1.5} separator="," />
-                                </Typography>
-                                <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
-                                <Typography variant="body2">No of confirmed COVID19 cases</Typography>
-                            </CardContent>
-                        </Grid>
-                        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                            <CardContent>
-                                <Typography color="textPrimary" gutterBottom>Recovered</Typography>
-                                <Typography variant="h5">
-                                    <CountUp start={0} end={data.recovered.value} duration={1.5} separator="," />
-                                </Typography>
-                                <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
-                                <Typography variant="body2">No of recoveries from COVID19</Typography>
-                            </CardContent>
-                        </Grid>
-                        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                            <CardContent>
-                                <Typography color="textPrimary" gutterBottom>Deaths</Typography>
-                                <Typography variant="h5">
-                                    <CountUp start={0} end={data.deaths.value} duration={1.5} separator="," />
-                                </Typography>
-                                <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
-                                <Typography variant="body2">No of deaths from COVID 19</Typography>
-                            </CardContent>
-                        </Grid>
-                    </Grid>
-                </div>
-
-            );
-
-        }
+  render() {
+    const { data } = this.props;
+    //Necessary to handle this using if else otherwise will get undefind error
+    if (!data.confirmed) {
+      return <h1>Loading Data, Please Wait...........</h1>;
+    } else {
+      return (
+        <Grid container spacing={1} justify="center" alignItems="center">
+          <Grid item component={Card} xs={12} md={3} elevation={4} className={cx(styles.card, styles.infected)}>
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                Confirmed
+              </Typography>
+              <Typography variant="h5">
+                <CountUp start={0} end={data.confirmed.value} duration={1.5} separator="," />
+              </Typography>
+              <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
+              <Typography variant="body2">No of confirmed COVID19 cases</Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card} xs={12} md={3} elevation={4} className={cx(styles.card, styles.recovered)}>
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                Recovered
+              </Typography>
+              <Typography variant="h5">
+                <CountUp start={0} end={data.recovered.value} duration={1.5} separator="," />
+              </Typography>
+              <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
+              <Typography variant="body2">No of recoveries from COVID19</Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card} xs={12} md={3} elevation={4} className={cx(styles.card, styles.deaths)}>
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                Deaths
+              </Typography>
+              <Typography variant="h5">
+                <CountUp start={0} end={data.deaths.value} duration={1.5} separator="," />
+              </Typography>
+              <Typography color="textSecondary">{new Date(data.lastUpdate).toDateString()}</Typography>
+              <Typography variant="body2">No of deaths from COVID 19</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      );
     }
+  }
 }
 
 export default Cards;
