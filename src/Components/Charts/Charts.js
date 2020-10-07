f2d2b3fceb280b408cdf148a0fa196445bb1fae6
Harshil Goel <harshilgoel96@gmail.com>
2020-10-07 21:47:25 +0530

    /api/daily api error handling

--- src/Components/Charts/Charts.js
+++ src/Components/Charts/Charts.js
@@ -1,92 +1,94 @@
-import React,{useState,useEffect} from 'react';
-import {fetchDailyData} from '../../api';
-import {Line,Bar} from 'react-chartjs-2';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 import styles from './Charts.module.css'
 
 
 
 
-const Charts = ({data,country}) => {
+const Charts = ({ data, country }) => {
 
-const [dailyData, setDailyData] = useState([]);
+    const [dailyData, setDailyData] = useState([]);
 
-const fetchdata = async () => {
-      
-      
-    const newdata = await fetchDailyData();
-    //console.log(newdata);
-    setDailyData(newdata);
-    
-}
+    const fetchdata = async () => {
 
-useEffect(() => {
-    
-    fetchdata();
-    
-    
-},[]);
-//console.log(dailyData)
 
-const filtereddata = dailyData.filter((item,i) => (i>=(dailyData.length-30) && i<(dailyData.length)));
+        const newdata = await fetchDailyData();
+        //console.log(newdata);
+        if (newdata) {
+            setDailyData(newdata);
+        }
 
-const lineChart = (
+    }
 
-    dailyData.length?<Line data={{
-    
+    useEffect(() => {
 
-        labels : filtereddata.map(({date}) => date),
-        datasets : [
-            {
-                label:'Infected',
-                data : filtereddata.map(({confirmed}) => confirmed),
-                fill : true,
-                borderColor: '#1976D2',
-                
-                  
-            },
-            {
-                label:'Deaths',
-                data : filtereddata.map(({deaths}) => deaths),
-                fill:true,
-                borderColor: '#F44336',  
-            }
-        ]
-    }} 
-    
-    
+        fetchdata();
 
-    />:null);
+
+    }, []);
+    //console.log(dailyData)
+
+    const filtereddata = dailyData.filter((item, i) => (i >= (dailyData.length - 30) && i < (dailyData.length)));
+
+    const lineChart = (
+
+        dailyData.length ? <Line data={{
+
+
+            labels: filtereddata.map(({ date }) => date),
+            datasets: [
+                {
+                    label: 'Infected',
+                    data: filtereddata.map(({ confirmed }) => confirmed),
+                    fill: true,
+                    borderColor: '#1976D2',
+
+
+                },
+                {
+                    label: 'Deaths',
+                    data: filtereddata.map(({ deaths }) => deaths),
+                    fill: true,
+                    borderColor: '#F44336',
+                }
+            ]
+        }}
+
+
+
+        /> : null);
 
     const barChart = (
 
-        (data.confirmed)?(<Bar data={{
-        labels:['Infected','Recovered','Deaths'],
-        datasets:[
-            {
-                label:'People',
-                data:[data.confirmed.value,data.recovered.value,data.deaths.value],
-                backgroundColor:['rgba(0, 4, 255, 0.6)','rgba(0, 255, 55, 0.6)','rgba(255, 0, 0, 0.6)'],
-                borderColor:['blue','green','red']
-            }
-        ]
-    }} 
-    
-    options={{
-        legend:{display:false,position:"right"},
-        title:{text:`Current Scenario in ${country}`,display:true}
-    }}
-    
-    
-    />):null);
-    
-    
+        (data.confirmed) ? (<Bar data={{
+            labels: ['Infected', 'Recovered', 'Deaths'],
+            datasets: [
+                {
+                    label: 'People',
+                    data: [data.confirmed.value, data.recovered.value, data.deaths.value],
+                    backgroundColor: ['rgba(0, 4, 255, 0.6)', 'rgba(0, 255, 55, 0.6)', 'rgba(255, 0, 0, 0.6)'],
+                    borderColor: ['blue', 'green', 'red']
+                }
+            ]
+        }}
+
+            options={{
+                legend: { display: false, position: "right" },
+                title: { text: `Current Scenario in ${country}`, display: true }
+            }}
 
 
-    return(
+        />) : null);
+
+
+
+
+    return (
         <div className={styles.container}>
-           {(country)?barChart:lineChart}
+            {(country) ? barChart : lineChart}
         </div>
-        );
+    );
 
 
 }
--- src/api/index.js
+++ src/api/index.js
@@ -4,20 +4,20 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async (country) => {
     let changableurl = url;
-    if(country){
+    if (country) {
         changableurl = `${url}/countries/${country}`;
     }
     try {
-        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(changableurl);
-        return {confirmed,recovered,deaths,lastUpdate};
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changableurl);
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
-        
+
     }
 }
 
 export const fetchDailyData = async () => {
     try {
-        const {data} = await axios.get(`${url}/daily`);
+        const { data } = await axios.get(`${url}/daily`);
         const modifiedData = data.map((item) => ({
             confirmed: item.confirmed.total,
             deaths: item.deaths.total,
@@ -26,21 +26,22 @@ export const fetchDailyData = async () => {
         //console.log(modifiedData)
         return modifiedData;
     } catch (error) {
-        
+        console.log("Error = " + error);
+
     }
 }
 
- export const fetchCountries = async () => {
+export const fetchCountries = async () => {
     try {
-        const {data:{countries}} = await axios.get(`${url}/countries`);
+        const { data: { countries } } = await axios.get(`${url}/countries`);
         const countryNames = countries.map((item) => item.name);
         //console.log(countryNames);
         return countryNames;
     }
-    catch(error){
+    catch (error) {
 
     }
 }
-           
-            
-    
+
+
+
